3c7c1c8e6dee3ef9437d064ba7b0db1fe82e31cf
ky0422 <ky0422a@gmail.com>
2023-02-21 11:08:45 +0900

    `update`: updated key

--- src/components/components/white.tsx
+++ src/components/components/white.tsx
@@ -2,7 +2,7 @@ import { KeyProps, KeyText } from '..'
 
 export default (props: KeyProps) => {
     return (
-        <div data-note={props.note} className='key var-width-4vw bg-background-light border-x-stone-700 border-solid border'>
+        <div data-note={props.note} className='key var-width-4vw bg-background-light border-[1.5px] border-black border-solid'>
             <KeyText>{props.text ?? props.note}</KeyText>
         </div>
     )
