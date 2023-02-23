5629022cded136534f16c262c0e2217e25133032
ky0422 <ky0422a@gmail.com>
2023-02-23 14:07:58 +0900

    `update`: updated keyboard

--- src/components/components/key.tsx
+++ src/components/components/key.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { create } from 'zustand'
 import { KeyText, PropsBase, Keyboard } from '..'
 import { Theme, ThemeContext } from '../../context/theme'
@@ -54,9 +54,69 @@ const onTextClick = (e: React.MouseEvent<HTMLElement, MouseEvent>, note: string)
     ;(document.getElementById(`${note}Key`) as HTMLDivElement).click()
 }
 
+interface KeyDown {
+    key: string
+    note: string
+}
+
+const keydown = (octave: number): Array<KeyDown> => [
+    { key: 'z', note: `C${octave - 1}` },
+    { key: 's', note: `C#${octave - 1}` },
+    { key: 'x', note: `D${octave - 1}` },
+    { key: 'd', note: `D#${octave - 1}` },
+    { key: 'c', note: `E${octave - 1}` },
+    { key: 'v', note: `F${octave - 1}` },
+    { key: 'g', note: `F#${octave - 1}` },
+    { key: 'b', note: `G${octave - 1}` },
+    { key: 'h', note: `G#${octave - 1}` },
+    { key: 'n', note: `A${octave - 1}` },
+    { key: 'j', note: `A#${octave - 1}` },
+    { key: 'm', note: `B${octave - 1}` },
+    { key: 'q', note: `C${octave}` },
+    { key: '2', note: `C#${octave}` },
+    { key: 'w', note: `D${octave}` },
+    { key: '3', note: `D#${octave}` },
+    { key: 'e', note: `E${octave}` },
+    { key: 'r', note: `F${octave}` },
+    { key: '5', note: `F#${octave}` },
+    { key: 't', note: `G${octave}` },
+    { key: '6', note: `G#${octave}` },
+    { key: 'y', note: `A${octave}` },
+    { key: '7', note: `A#${octave}` },
+    { key: 'u', note: `B${octave}` },
+    { key: 'i', note: `C${octave + 1}` },
+    { key: '9', note: `C#${octave + 1}` },
+    { key: 'o', note: `D${octave + 1}` },
+    { key: '0', note: `D#${octave + 1}` },
+    { key: 'p', note: `E${octave + 1}` },
+    { key: '[', note: `F${octave + 1}` },
+    { key: '=', note: `F#${octave + 1}` },
+    { key: ']', note: `G${octave + 1}` },
+    { key: '.', note: `G#${octave + 1}` },
+    { key: ';', note: `A${octave + 1}` },
+    { key: '/', note: `A#${octave + 1}` },
+    { key: "'", note: `B${octave + 1}` },
+]
+
 export default ({ type, note, text }: Props) => {
     const { toggle } = React.useContext(ThemeContext) as Theme
     const volume = useVolumeStore((state) => state.volume / 100)
+
+    useEffect(() => {
+        const callback = (e: KeyboardEvent) => {
+            if (e.repeat) return
+            const key = keydown(4).find((k) => k.key === e.key)
+            if (key) {
+                const element = document.getElementById(`${key.note}Key`) as HTMLDivElement
+                element.click()
+            }
+        }
+        document.addEventListener('keydown', callback)
+
+        return () => {
+            document.removeEventListener('keydown', callback)
+        }
+    })
 
     if (type === Keyboard.KeyType.White) {
         return (
